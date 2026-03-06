84929c618d7fa5d3a83e0886963df0b21ffab0f0
Vishaal Ravichandran <vishaalravichandran369@gmail.com>
2026-03-07 03:23:39 +0530

    Update teamControllers.js
    Thoonga poren

--- back-end/controllers/teamControllers.js
+++ back-end/controllers/teamControllers.js
@@ -5,7 +5,7 @@ const Team = require("../models/Team");
 exports.getTeamDashboard = async (req, res) => {
 
   const team = await Team.findById(
-    req.team.id
+    req.team.teamId
   ).select("-leader.password");
 
   res.json(team);
